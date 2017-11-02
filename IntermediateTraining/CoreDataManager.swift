706daeb9830a342e31cf276f65d2363ba9c5292e
Brian Voong <bhlvoong@gmail.com>
2017-11-01 18:41:33 -0700

    Ep 21 Form Validation

--- IntermediateTraining/CoreDataManager.swift
+++ IntermediateTraining/CoreDataManager.swift
@@ -35,7 +35,7 @@ struct CoreDataManager {
         }
     }
     
-    func createEmployee(employeeName: String, company: Company) -> (Employee?, Error?) {
+    func createEmployee(employeeName: String, birthday: Date, company: Company) -> (Employee?, Error?) {
         let context = persistentContainer.viewContext
         
         //create an employee
@@ -54,6 +54,7 @@ struct CoreDataManager {
         let employeeInformation = NSEntityDescription.insertNewObject(forEntityName: "EmployeeInformation", into: context) as! EmployeeInformation
         
         employeeInformation.taxId = "456"
+        employeeInformation.birthday = birthday
         
 //        employeeInformation.setValue("456", forKey: "taxId")
         
